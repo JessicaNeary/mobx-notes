d0f7ac6cbb1f7815e950dd4f4d8778e75469a791
Jess Neary <Caneary@hotmail.com>
2017-04-27 10:53:23 +1200

    Updated readme info and converted App to functional component

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router'
 
-export default React.createClass({
-  render () {
-    return (
-      <div>
-        <h1>Title</h1>
-        {this.props.children}
-      </div>
-    )
-  }
-})
+const App = props => {
+  return (
+    <div>
+      <h1>Title</h1>
+      {props.children}
+    </div>
+  )
+}
+
+export default App
